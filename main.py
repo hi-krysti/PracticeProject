0332042ab2250aafc35e37741f278950fc46b23a
James Barnes <james.barnes11998@gmail.com>
2023-11-06 20:15:54 -0500

    My Changes

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("There once was boy named Steve and he wanted a pet dog. So he went to the pound.")
+print("There once was boy named Steve and he wanted a pet Zebra. So he went to the pound.")
+print("He quickly realized there were no Zebras in the local pound.")
